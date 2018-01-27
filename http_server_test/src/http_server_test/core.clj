76d3f01864b6bf6e38bb853927ed2fa7b9ea6906
Neil McPhail <NMcPhail@eu.wlgore.com>
2018-01-27 17:19:23 +0000

    mplemente some stuff - added tests

--- http_server_test/src/http_server_test/core.clj
+++ http_server_test/src/http_server_test/core.clj
@@ -1,13 +1,23 @@
  (ns http-server-test.core
     (:require  
      [org.httpkit.server :as serv ]
-      ))
+     [clojure.pprint :as pp]
+     [clojure.java.io :as io]))
 
 
 (defn app [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "hello HTTP!"})
+  (def response-template  {:status  200
+                           :headers {"Content-Type" "text/html"}
+                           :body    "hello HTTP!"})
+
+  (with-open [rdr (io/reader (:body req ) :encoding "UTF-8"   )]
+    (let [s (slurp rdr )]
+      (assoc response-template :body  ( str (eval (read-string  s))))
+      #_(pp/pprint s)
+      #_(assoc response-template :body s)
+      )))
+
+  
 
 (defonce server (atom nil))
 
@@ -18,7 +28,7 @@
     (reset! server nil))
 
 
-;;(reset! server (serv/run-server app {:port 8090}))
+(reset! server (serv/run-server app {:port 8090}))
 
 
 
--- http_server_test/test/http_server_test/core_test.clj
+++ http_server_test/test/http_server_test/core_test.clj
@@ -1,7 +1,43 @@
 (ns http-server-test.core-test
   (:require [clojure.test :refer :all]
-            [http-server-test.core :refer :all]))
+            [http-server-test.core :refer :all]
+            [org.httpkit.client :as http]
+              [clojure.pprint :as pp]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+    (let [ options
+            #_{:body (str '(http-server-test.data-access/get-user-by-id 1))}
+          {:body  (str '(http-server-test.data-access/get-user-by-id 1))}
+            #_{:form-params {:name "http-kit" :features ["async" "client" "server"]}}
+          {:keys [status error] :as res } @(http/post "http://localhost:8090"  options)]
+     (if error
+       (println "Failed, exception is " error)
+       (pp/pprint
+        (eval (read-string (:body  res))) )))
+    (is (= 1 1))))
+
+
+(deftest b-test
+  (testing "FIXME, I fail."
+
+
+    (let [ options
+            #_{:body (str '(http-server-test.data-access/get-user-by-id 1))}
+          {:body  (str '(http-server-test.data-access/get-entities-user 1))}
+            #_{:form-params {:name "http-kit" :features ["async" "client" "server"]}}
+          {:keys [status error] :as res } @(http/post "http://localhost:8090"  options)]
+     (if error
+       (println "Failed, exception is " error)
+       (pp/pprint
+        (eval (read-string (:body  res))) )))
+    (is (= 1 1))))
+
+
+
+
+
+
+(run-tests)
